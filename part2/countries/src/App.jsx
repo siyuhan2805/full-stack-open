6a2ded064e180cc9111fc1d0a76e2286289c25b5
Henry <siyu.han2805@gmail.com>
2024-10-19 17:58:41 +1100

    Complete countries exercise 2.20 part step 3

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -12,7 +12,7 @@ const Filter = ({filter, onChange}) => {
   )
 }
 
-const Countries = ({filteredCountries, filter}) => {
+const Countries = ({filteredCountries, filter, showCountryInfo, showWeatherInfo, handleCapitalChange}) => {
   const countries = filteredCountries()
   if (filter === '') {
     <div></div>
@@ -27,11 +27,13 @@ const Countries = ({filteredCountries, filter}) => {
   else if (countries.length < 10 && countries.length > 1) {
     return (
       <div>
-        {countries.map(country => <div key={country.name.common}>{country.name.common}</div>)}
+        {countries.map(country => <div key={country.name.common}>{country.name.common}<button type="button" value={country.name.common} onClick={showCountryInfo}>show</button></div>)}
       </div>
     )  
   }
   else if (countries.length === 1) {
+    handleCapitalChange(countries[0].capital[0])
+    const weatherIcon = `https://openweathermap.org/img/wn/${showWeatherInfo.list[0].weather[0].icon}@2x.png`
     return (
       <div>
         <h1>{countries[0].name.common}</h1>
@@ -42,6 +44,10 @@ const Countries = ({filteredCountries, filter}) => {
           {Object.keys(countries[0].languages).map(key => <li>{countries[0].languages[key]}</li>)}
         </ul>
         <img src={countries[0].flags.png}/>
+        <h1>Weather in {countries[0].capital}</h1>
+        <p>temperature {showWeatherInfo.list[0].main.temp} Celsius </p>
+        <img src={weatherIcon}/>
+        <p>wind {showWeatherInfo.list[0].wind.speed} m/s</p>
       </div>
     )
   }
@@ -51,6 +57,8 @@ const Countries = ({filteredCountries, filter}) => {
 const App = () => {
   const [newFilter, setNewFilter] = useState('')
   const [countries, setCountries] = useState([])
+  const [capital, setCapital] = useState('Helsinki')
+  const [weatherInfo, setWeatherInfo] = useState({})
 
   useEffect(() => {
     services.getAllCountries()
@@ -58,6 +66,13 @@ const App = () => {
       setCountries(allCountries)
     })
   }, [])
+
+  useEffect(() => {
+    services.getCapitalWeather(capital)
+    .then(weatherInfo => {
+      setWeatherInfo(weatherInfo)
+    })
+  }, [capital])
 
   // method for applying filter to countries state and returning only filtered countries
   const filteredCountries = () => {
@@ -68,6 +83,17 @@ const App = () => {
     console.log(filteredCountries)
     return filteredCountries
   }
+
+
+
+  // method for showing the country view when clicked
+  const showCountryInfo = (event) => {
+    event.preventDefault()
+    setNewFilter(event.target.value)
+  }
+
+  // method for handling capital change
+  const handleCapitalChange = capital => setCapital(capital)
   
   // method for handling filter changes and assigning newFilter state
   const handleFilterChange = (event) => {
@@ -78,7 +104,7 @@ const App = () => {
   return (
     <div>
       <Filter filter={newFilter} onChange={handleFilterChange}/>
-      <Countries filteredCountries={filteredCountries} filter={newFilter}/>
+      <Countries filteredCountries={filteredCountries} filter={newFilter} showCountryInfo={showCountryInfo} showWeatherInfo={weatherInfo} handleCapitalChange={handleCapitalChange}/>
 
     </div>
   )
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/'
+const weatherApiBaseURL = `https://api.openweathermap.org/data/2.5/forecast?`
+const apiKey = import.meta.env.VITE_API_KEY
 
 const getAllCountries = () => {
     const request = axios.get(`${baseUrl}/all`)
     return request.then(response => response.data)
 }
 
+const getCapitalWeather = (cityName) => {
+    const request = axios.get(`${weatherApiBaseURL}q=${cityName}&appid=${apiKey}&units=metric`)
+    return request.then(response => response.data)
+}
+
+
+
 export default {
-    getAllCountries
+    getAllCountries, getCapitalWeather
 }
